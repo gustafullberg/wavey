c74b822c38793a321b76ce04280643cb267b0985
Gustaf Ullberg <gustaf.ullberg@gmail.com>
2020-10-07 19:25:48 +0200

    Bugfixes

--- gui.cpp
+++ gui.cpp
@@ -164,8 +164,10 @@ bool Gui::ButtonPress(GdkEventButton* button_event) {
             state->selection_end = -1.f;
             mouse_down = true;
         } else if (button_event->type == GDK_2BUTTON_PRESS) {
-            state->selection_start = 0.f;
-            state->selection_end = state->tracks[state->selected_track].audio_buffer->Length();
+            if (state->tracks.size()) {
+                state->selection_start = 0.f;
+                state->selection_end = state->tracks[state->selected_track].audio_buffer->Length();
+            }
         }
         glarea.queue_render();
     }
@@ -174,7 +176,7 @@ bool Gui::ButtonPress(GdkEventButton* button_event) {
 
 bool Gui::ButtonRelease(GdkEventButton* button_event) {
     if (button_event->button == 1) {
-        if (button_event->type == GDK_BUTTON_PRESS) {
+        if (button_event->type == GDK_BUTTON_RELEASE) {
             const float time = state->zoom_window.GetTime(button_event->x / win_width);
             if (time > state->selection_start) {
                 state->selection_end = time;

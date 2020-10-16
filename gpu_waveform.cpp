ed116d68051c5ba7c0758408ae7a8202b79e7cee
Gustaf Ullberg <gustaf.ullberg@gmail.com>
2020-10-16 21:36:08 +0200

    LOD fixes

--- gpu_waveform.cpp
+++ gpu_waveform.cpp
@@ -4,17 +4,17 @@
 #include <iostream>
 
 namespace {
-void MakeLowResBuffer(std::vector<float>& buffer, const AudioBuffer& ab) {
+void MakeLowResBuffer(std::vector<float>& buffer, const AudioBuffer& ab, const int down_sampling_factor) {
     const int num_channels = ab.NumChannels();
     const int num_vertices = ab.NumFrames();
     const float* samples = ab.Samples();
 
     // Each channel has 2 output samples for every 1000 input samples.
-    buffer.resize(2 * num_channels * ((num_vertices + 999) / 1000));
+    buffer.resize(2 * num_channels * ((num_vertices + down_sampling_factor-1 ) / down_sampling_factor));
 
     int dest_idx = 0;
-    for (int i = 0; i < num_vertices; i += 1000) {
-        const int end = std::min(i + 1000, num_vertices);
+    for (int i = 0; i < num_vertices; i += down_sampling_factor) {
+        const int end = std::min(i + down_sampling_factor, num_vertices);
         for (int c = 0; c < num_channels; c++) {
             float min = 1.f;
             float max = -1.f;
@@ -47,7 +47,7 @@ GpuWaveform::GpuWaveform(const AudioBuffer& ab) {
     glVertexAttribPointer(0, 1, GL_FLOAT, GL_FALSE, 0, nullptr);
 
     std::vector<float> buffer_lod;
-    MakeLowResBuffer(buffer_lod, ab);
+    MakeLowResBuffer(buffer_lod, ab, 1000);
     num_vertices_lod = buffer_lod.size() / num_channels;
 
     glGenBuffers(1, &vbo_lod);
@@ -73,13 +73,11 @@ void GpuWaveform::Draw(int channel, float start_time, float end_time, bool low_r
             std::max(static_cast<int>(std::floor(start_time * samplerate / 1000.f * 2.f)), 0);
         const int end_index = std::min(
             static_cast<int>(std::ceil(end_time * samplerate / 1000.f * 2.f)), num_vertices_lod);
-        // int start_index = 0;
-        // int end_index = num_vertices_lod;
         glBindVertexArray(vao);
         glBindBuffer(GL_ARRAY_BUFFER, vbo_lod);
         glVertexAttribPointer(0, 1, GL_FLOAT, GL_FALSE, num_channels * sizeof(float),
                               (const void*)(channel * sizeof(float)));
-        glDrawArrays(GL_LINES, start_index, end_index - start_index);
+        glDrawArrays(GL_LINE_STRIP, start_index, end_index - start_index);
     } else {
         const int start_index = std::max(static_cast<int>(std::floor(start_time * samplerate)), 0);
         const int end_index =

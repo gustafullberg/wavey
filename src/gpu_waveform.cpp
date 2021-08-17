71c055378500cd1ae4ce87add4c7a4534a34999f
Gustaf Ullberg <gustaf.ullberg@gmail.com>
2021-08-17 15:21:00 +0200

    Waveform rendering fix

--- src/gpu_waveform.cpp
+++ src/gpu_waveform.cpp
@@ -77,16 +77,19 @@ void GpuWaveform::Draw(int channel,
                        bool draw_low_res,
                        bool draw_points) {
     const float rate = draw_low_res ? samplerate / 1000.f * 2.f : samplerate;
+    const int last_index = (draw_low_res ? num_vertices_lod : num_vertices) - 1;
     const int start_index = std::max(static_cast<int>(std::floor(start_time * rate)), 0);
-    const int end_index =
-        std::min(start_index + static_cast<int>(std::ceil((end_time - start_time) * rate)),
-                 num_vertices - 1);
+    const int end_index = std::min(
+        start_index + static_cast<int>(std::ceil((end_time - start_time) * rate)), last_index);
 
-    glBindVertexArray(vao);
-    glBindBuffer(GL_ARRAY_BUFFER, draw_low_res ? vbo_lod : vbo);
-    glVertexAttribPointer(0, 1, GL_FLOAT, GL_FALSE, num_channels * sizeof(float),
-                          (const void*)((start_index * num_channels + channel) * sizeof(float)));
-    glDrawArrays(draw_points ? GL_POINTS : GL_LINE_STRIP, 0, end_index - start_index + 1);
+    if (end_index >= start_index) {
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ARRAY_BUFFER, draw_low_res ? vbo_lod : vbo);
+        glVertexAttribPointer(
+            0, 1, GL_FLOAT, GL_FALSE, num_channels * sizeof(float),
+            (const void*)((start_index * num_channels + channel) * sizeof(float)));
+        glDrawArrays(draw_points ? GL_POINTS : GL_LINE_STRIP, 0, end_index - start_index + 1);
+    }
 }
 
 void GpuWaveform::DrawLines(int channel, float start_time, float end_time, bool draw_low_res) {
